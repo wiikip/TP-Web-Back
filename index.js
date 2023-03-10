0ea7785e2f251cd2bea2f6623c0b575e105e1a12
Kevin Fairise <kevin.fairise25@gmail.com>
2023-03-10 14:15:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express'); //import de la bibliothèque Express
+var app = express(); //instanciation d'une application Express
+
+// Pour s'assurer que l'on peut faire des appels AJAX au serveur
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// Ici faut faire faire quelque chose à notre app...
+// On va mettre les "routes"  == les requêtes HTTP acceptéés par notre application.
+
+
+
+app.listen(8080); //commence à accepter les requêtes
+console.log("App listening on port 8080...");
+
